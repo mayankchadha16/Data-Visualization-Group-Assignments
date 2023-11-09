aa86116616b64439c65f7234974d03aa005f4054
mayankchadha16 <chadhamayank1609@gmail.com>
2023-11-09 16:30:05 +0530

    wrap up treemaps

--- A2/InfoViz/Treemap/script2.js
+++ A2/InfoViz/Treemap/script2.js
@@ -1,15 +1,53 @@
-d3.csv('make.csv', function(err, rows){
+d3.csv('model.csv', function (err, rows) {
   function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]});
-}
+      return rows.map(function (row) {
+          return row[key];
+      });
+  }
 
-var data = [{
-      type: "treemap",
-      ids: unpack(rows, 'name'),
-      values: unpack(rows, 'value'),
-      labels: unpack(rows, 'name'),
-      parents: unpack(rows, 'parent')
-    }];
+  function createTreemap(divId, regionRows, region) {
+      var data = [{
+          type: "treemap",
+          ids: unpack(regionRows, 'name'),
+          values: unpack(regionRows, 'value'),
+          labels: unpack(regionRows, 'name'),
+          textinfo: "label+value",
+          parents: unpack(regionRows, 'parent'),
+          marker: {
+              colorscale: 'Blues', 
+              colorbar: {
+                  tickvals: [0, 1, 2, 3, 4], 
+                  ticktext: ['Central', 'Southern', 'Northern', 'Western', 'Eastern'], 
+              }
+          }
+      }];
 
-Plotly.newPlot('myDiv', data);
-})
+      Plotly.newPlot(divId, data);
+  }
+
+  var centralRows = rows.filter(function (row) {
+      return row.parent === 'Central';
+  });
+
+  var easternRows = rows.filter(function (row) {
+      return row.parent === 'Eastern';
+  });
+
+  var northernRows = rows.filter(function (row) {
+      return row.parent === 'Northern';
+  });
+
+  var southernRows = rows.filter(function (row) {
+      return row.parent === 'Southern';
+  });
+
+  var westernRows = rows.filter(function (row) {
+      return row.parent === 'Western';
+  });
+
+  createTreemap('centralDiv', centralRows, 'Central');
+  createTreemap('easternDiv', easternRows, 'Eastern');
+  createTreemap('northernDiv', northernRows, 'Northern');
+  createTreemap('southernDiv', southernRows, 'Southern');
+  createTreemap('westernDiv', westernRows, 'Western');
+});
